1b9101db0239c56b6ec2d8172ac6aa6e27fbb3b6
robertpanvip <robertpanvip@163.com>
2026-08-05 15:06:51 +0800

    Add favicon to HTML configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,7 @@ import { pluginReact } from "@rsbuild/plugin-react";
 export default defineConfig({
   html: {
     title: 'Pan Open Source',
+    favicon:'./public/logo.svg'
   },
   plugins: [pluginReact()],
 });
